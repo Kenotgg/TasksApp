b9651084c0ed628bf4ca06247f5e921d8b96217b
Kenotgg <130081795+Kenotgg@users.noreply.github.com>
2025-05-07 14:34:27 +0500

    First

---
+++ TasksApp.Server/Controllers/TasksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TasksApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet("getTask")]
+        public IActionResult Get() 
+        {
+            return Ok();
+        }
+    }
+}
